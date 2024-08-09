c2e98c1d437bc515ec210b42cd6418255c8ede98
Far Bear <asafa020@gmail.com>
2024-08-09 12:56:06 +0300

    Increased sound lerp duration from video to Main screen.
    1 => 1.5

--- Life and Death/Assets/Scripts/Lerper.cs
+++ Life and Death/Assets/Scripts/Lerper.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 using UnityEngine.Events;
 public class Lerper : MonoBehaviour
 {
-    [SerializeField] private float lerpDuration = 1;
+    [SerializeField] private float lerpInDuration = 1;
+    [SerializeField] private float lerpOutDuration = 1;
     [SerializeField] UnityEvent<float> OnLerpValueChange;
     private float lerpValue = 0;
     private float timer = 0;
     public void ZeroToValue(float value)
     {
-        StartCoroutine(DoLerp(0, value));
+        StartCoroutine(DoLerp(0, value, lerpInDuration));
 
     }
     public void ValueToZero(float value)
     {
-        StartCoroutine(DoLerp(value, 0));
+        StartCoroutine(DoLerp(value, 0, lerpOutDuration));
     }
-    private IEnumerator DoLerp(float startValue, float endValue)
+    private IEnumerator DoLerp(float startValue, float endValue, float lerpDuration)
     {
         //yield return new WaitForSeconds(delay);
         timer = 0;
